13a72477fbddd6b8dda8a93825dc2c717a66a4eb
Ham Vocke <hermann.vocke@gmail.com>
2017-09-28 20:59:17 +0200

    build pdf and epub version in build script

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
 
-asciidoctor --doctype book testing-microservices.adoc
+echo "Generating epub version"
+asciidoctor -d book -b docbook5 testing-microservices.adoc -o output.docbook
+pandoc -f docbook -t epub output.docbook -o testing-microservices.epub
+
+echo "Generating pdf version"
+asciidoctor-pdf -d book testing-microservices.adoc -o testing-microservices.pdf
+
+echo "Cleanup output"
+rm output.docbook
